9ddc4873334d7717ad0626445ecc3014faa127bd
Antonio Contreras <antoniocontrerasroman@gmail.com>
2025-08-20 12:54:26 +0200

    add env variable

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { createClient } from "@content-island/api-client";
 import type { Product, Language } from "./model";
 
 const client = createClient({
-  accessToken: "e9b9fb44824c4ed921376e5f2130c71e", // Replace with your actual token
+  accessToken: import.meta.env.VITE_CONTENT_ISLAND_SECRET_TOKEN, // Replace with your actual token
 });
 
 export const getProducts = async (language: Language): Promise<Product[]> => {
